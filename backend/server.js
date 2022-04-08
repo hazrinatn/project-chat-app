e4904aa2e24a8c9d9bb6bea3158fc757c45257fa
hazrinatn <hazrinatn@gmail.com>
2022-04-08 13:09:12 +0700

    Add error handlers

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const chats = require("./data/data");
 const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler} = require("./middleware/errorMiddleware")
 
 
 dotenv.config();
@@ -15,8 +16,12 @@ app.get("/", (req, res) => {
     res.send("API is running successfully.")
 });
 
-app.use('/api/user', userRoutes)
+app.use('/api/user', userRoutes);
 
-const PORT = process.env.PORT || 5000
+app.use(notFound);
+app.use(errorHandler);
+
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
