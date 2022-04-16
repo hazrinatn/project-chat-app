79f78b6acd3519f010b68f639584f41a4777b585
hazrinatn <hazrinatn@gmail.com>
2022-04-16 23:54:39 +0700

    Add real-time functionality

--- backend/server.js
+++ backend/server.js
@@ -30,4 +30,39 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
+const server = app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
+
+const io = require('socket.io')(server, {
+    pingTimeoUT: 60000,
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("Connected to socket.io");
+
+    socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        console.log(userData._id);
+        socket.emit("connected");
+    });
+
+    socket.on("join chat", (room) => {
+        socket.join(room);
+        console.log("User Joined Room: " + room);
+    });
+    socket.on("new message", (newMessageReceived) => {
+        let chat = newMessageReceived.chat;
+
+        if (!chat.users) return console.log("chat.users not defined");
+
+        chat.users.forEach(user => {
+            if (user._id == newMessageReceived.sender._id) return;
+
+            socket.in(user._id).emit("message received", newMessageReceived);
+        })
+        
+        
+    })
+});
