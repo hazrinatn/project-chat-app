a8dea0b64d64aecb0c63747b7325729823a52b5e
hazrinatn <hazrinatn@gmail.com>
2022-04-17 20:28:05 +0700

    Add socket.off

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,7 @@ io.on("connection", (socket) => {
         socket.join(room);
         console.log("User Joined Room: " + room);
     });
+
     socket.on("typing", (room) => socket.in(room).emit("typing"));
     socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
@@ -66,7 +67,10 @@ io.on("connection", (socket) => {
 
             socket.in(user._id).emit("message received", newMessageReceived);
         })
-        
-        
-    })
+    });
+
+    socket.off("setup", () => {
+        console.log("USER DISCONNECTED");
+        socket.leave(userData._id);
+    });
 });
