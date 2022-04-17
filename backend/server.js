755f076d9c9641fb4f5acc12a9189806c3a2146b
hazrinatn <hazrinatn@gmail.com>
2022-04-17 20:22:30 +0700

    Add typing functionality

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,10 @@ io.on("connection", (socket) => {
         socket.join(room);
         console.log("User Joined Room: " + room);
     });
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+
     socket.on("new message", (newMessageReceived) => {
         let chat = newMessageReceived.chat;
 
